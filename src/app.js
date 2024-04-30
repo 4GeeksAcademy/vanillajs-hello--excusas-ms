226e92883d07186d287e3ae46099044065b6f73f
matii17 <154836261+matii17@users.noreply.github.com>
2024-04-30 01:10:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuseGen();
 };
+
+function excuseGen() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let who1 = Math.floor(Math.random() * who.length);
+  let action1 = Math.floor(Math.random() * action.length);
+  let what1 = Math.floor(Math.random() * what.length);
+  let when1 = Math.floor(Math.random() * when.length);
+  return `${who[who1]}${action[action1]}${what[what1]}${when[when1]}`;
+}
